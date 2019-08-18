6067c244cb38f94be151b2ad616e8f747ebc866d
Adrian Maulana Muhammad <adriantoto7@gmail.com>
2019-08-18 15:26:32 +0700

    redesign tab layout

--- app/src/main/java/dicoding/adrian/submission4/Favorite/FavoriteFragment.java
+++ app/src/main/java/dicoding/adrian/submission4/Favorite/FavoriteFragment.java
@@ -6,7 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -39,11 +39,13 @@ public class FavoriteFragment extends Fragment {
 
         // Tab Configuration
         TabLayout tabLayoutFavorite = view.findViewById(R.id.tablayout_favorite);
-        ViewPager viewPagerFavorite = view.findViewById(R.id.viewpager_favorite);
+        NonSwipeableViewPager viewPagerFavorite = view.findViewById(R.id.viewpager_favorite);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+
         // Adding Fragments
         adapter.AddFragment(new MovieFavoriteFragment(), getString(R.string.movie));
         adapter.AddFragment(new TvFavoriteFragment(), getString(R.string.tv_show));
+
         // Adapter Setup
         viewPagerFavorite.setAdapter(adapter);
         tabLayoutFavorite.setupWithViewPager(viewPagerFavorite);
